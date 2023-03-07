6186877ab0da98168323a116a1f21df841b4a484
SAHIL SANJAY CHALKE <SahilCHALKE1011@gmail.com>
2023-03-08 01:42:21 +0530

    new commit/7/3/2023

---
+++ food/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Item(models.Model):
+    item_name = models.CharField(max_length=100)
+    item_desc =models.CharField(max_length=200)
+    item_price =models.IntegerField()
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.item_name
+        
+
+
+
+
---
+++ food/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name='food'
+urlpatterns = [
+    
+    path('',views.index,name="index"),
+    path('item/',views.item,name='item'),
+    #food/1
+    path('<int:item_id>/',views.detail,name="detail"),
+]
+

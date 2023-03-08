1b95640285acdcd58d52b9a695b0419769e648e7
SAHIL SANJAY CHALKE <SahilCHALKE1011@gmail.com>
2023-03-09 01:21:15 +0530

    new commit/8/3/2023-2

--- food/urls.py
+++ food/urls.py
@@ -8,5 +8,12 @@ urlpatterns = [
     path('item/',views.item,name='item'),
     #food/1
     path('<int:item_id>/',views.detail,name="detail"),
+    #Add items
+    path('add/',views.create_item,name="create_item"),
+    #update
+    path('update/<int:id>/',views.update_item,name="update_item"),
+    #delete
+    path('delete/<int:id>/',views.delete_item,name="delete_item"),
+
 ]
 
--- foodsite/urls.py
+++ foodsite/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/',include('food.urls')),
+    path('register/',user_views.register,name='register'),
+
 ]

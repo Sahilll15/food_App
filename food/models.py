9c6cd1a2ffe7043fdb0814476d2267fa5dd99102
SAHIL SANJAY CHALKE <SahilCHALKE1011@gmail.com>
2023-03-08 22:45:10 +0530

    new commit/8/3/2023

--- food/models.py
+++ food/models.py
@@ -5,8 +5,9 @@ class Item(models.Model):
     item_name = models.CharField(max_length=100)
     item_desc =models.CharField(max_length=200)
     item_price =models.IntegerField()
+    item_image = models.CharField(max_length=500,default="https://livingstonbagel.com/wp-content/uploads/2016/11/food-placeholder.jpg")
 
-    objects = models.Manager()
+    
 
     def __str__(self):
         return self.item_name
